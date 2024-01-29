22400971da7cf857a5117be6f257d9b2c1044bd6
Baspa <hello@baspa.dev>
2024-01-29 17:05:54 +0100

    Remove unnecessary code

--- src/TimezonesServiceProvider.php
+++ src/TimezonesServiceProvider.php
@@ -10,16 +10,7 @@ class TimezonesServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
-            ->name('laravel-timezones')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-timezones_table')
-            ->hasCommand(TimezonesCommand::class);
+            ->name('laravel-timezones');
     }
 }
