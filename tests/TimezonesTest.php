bf004fde66555ea5497c5efbb97902482496ef5e
Baspa <10845460+Baspa@users.noreply.github.com>
2025-02-26 14:37:57 +0000

    Fix styling

--- tests/TimezonesTest.php
+++ tests/TimezonesTest.php
@@ -1,31 +1,31 @@
 <?php
 
 it('can get a grouped array of timezones', function () {
-    $timezones = new \Baspa\Timezones\Timezones();
+    $timezones = new \Baspa\Timezones\Timezones;
 
     $this->assertIsArray($timezones->toArray(grouped: true));
 });
 
 it('can get a grouped array of timezones with html entities', function () {
-    $timezones = new \Baspa\Timezones\Timezones();
+    $timezones = new \Baspa\Timezones\Timezones;
 
     $this->assertIsArray($timezones->toArray(grouped: true, htmlencode: true));
 });
 
 it('can get a grouped array of timezones without html entities', function () {
-    $timezones = new \Baspa\Timezones\Timezones();
+    $timezones = new \Baspa\Timezones\Timezones;
 
     $this->assertIsArray($timezones->toArray(grouped: true, htmlencode: false));
 });
 
 it('can get a flat array of timezones', function () {
-    $timezones = new \Baspa\Timezones\Timezones();
+    $timezones = new \Baspa\Timezones\Timezones;
 
     $this->assertIsArray($timezones->toArray(grouped: false));
 });
 
 it('can exclude continents', function () {
-    $timezones = new \Baspa\Timezones\Timezones();
+    $timezones = new \Baspa\Timezones\Timezones;
 
     $excludedTimezones = $timezones->excludeContinents(['Africa', 'America'])->toArray(grouped: false);
 
@@ -35,7 +35,7 @@ it('can exclude continents', function () {
 });
 
 it('can include general timezones', function () {
-    $timezones = new \Baspa\Timezones\Timezones();
+    $timezones = new \Baspa\Timezones\Timezones;
 
     $includedTimezones = $timezones->includeGeneral()->toArray(grouped: false);
 
